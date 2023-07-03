12a0cb39da8d6dc231f9a2cfadcef870da40d25a
Danny Schantz <dschantz801@gmail.com>
2023-07-03 16:39:58 +0800

    reformated

---
+++ app.js
@@ -0,0 +1,16 @@
+import express from "express"
+import usersRouter from "./src/controllers/users.controllers.js"
+import authRouter from "./src/controllers/auth.controllers.js"
+import auth from "./src/middlewares/auth.js"
+import cors from 'cors'
+import morgan from "morgan"
+
+const app = express()
+app.use(morgan('combined'))
+app.use(express.json())
+app.use(cors())
+
+app.use('/users', usersRouter)
+app.use('/auth', authRouter)
+
+export default app
