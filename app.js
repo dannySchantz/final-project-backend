83425339ee9373472dfe0a970c30909b984aac88
LimShuonle <srxmax161@gmail.com>
2023-07-11 15:43:18 +0800

    created auth refresh and added controller to app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from "express"
 import usersRouter from "./src/controllers/users.controllers.js"
 import authRouter from "./src/controllers/auth.controllers.js"
 import postsRouter from "./src/controllers/posts.controllers.js"
+import authrefreshRotuer from "./src/controllers/auth-refresh.controller.js"
 import auth from "./src/middlewares/auth.js"
 import cors from 'cors'
 import morgan from "morgan"
@@ -14,5 +15,6 @@ app.use(cors())
 app.use('/users', usersRouter)
 app.use('/auth', authRouter)
 app.use('/posts', postsRouter)
+app.use('/auth-refresh', authrefreshRotuer)
 
 export default app
